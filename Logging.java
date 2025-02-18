b2e82428b117f1800e8f15236e93cdd103cb1596
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-02-18 03:20:01 +0000

    Done java and linting erorrs logging

--- Logging.java
+++ Logging.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 
-/**
-* This program will calculate the maximum amount of logs that can fit into a truck based on certain lengths.
-*
-* @author  Remy Skelton
-* @version 1.0
-* @since   2025-February-14
-*/
+/**.
+ * This program will calculate the maximum amount of logs
+ * that can fit into a truck based on certain lengths.
+ *
+ * @author  Remy Skelton
+ * @version 1.0
+ * @since   2025-February-14
+ */
 
 final class Logging {
-    // Constants
-    final int MAX_WEIGHT = 1100;
-
+    /**.
+     * Constants
+     */
+    private static final int MAX_WEIGHT = 1100;
 
     /**
-     * This is a private constructor used to satisfy the
-     * style checker.
+     * This is a private constructor used to satisfy the style checker.
      *
      * @exception IllegalStateException Utility class.
      * @see IllegalStateException
-    */
+     */
     private Logging() {
         throw new IllegalStateException("Utility class");
     }
@@ -31,47 +32,50 @@ final class Logging {
      */
     public static void main(final String[] args) {
         // Welcome message
-        System.out.print("This program will calculate ");
-        System.out.print("the maximum number of logs that can fit into");
-        System.out.println(" a truck based on certain lengths.");
+        System.out.println("This program will calculate the maximum"
+        + "number of logs that can fit into a truck based on certain lengths.");
 
-        // Ask user to enter the log lengths
-        System.out.println("Please enter the log lengths you will be using: ");
+        // Use a scanner to get the log length from the user
+        try (Scanner scanner = new Scanner(System.in)) {
+            double logLengthDouble;
 
-        // Use a scanner to get the log length from user
-        final Scanner scanner = new Scanner(System.in);
-        final String logLengthString = scanner.nextLine();
+            // Keep asking for input until a valid log length is entered
+            do {
+                // Message for input
+                System.out.println("Please enter the log length you"
+                + "will be using (0.25, 0.5, or 1): ");
+                String logLengthString = scanner.nextLine();
 
-        // Try catch to catch users invalid input
-        try {
-            // convert the string to a double
-            final double logLengthDouble = Double.parseDouble(logLengthString);
+                try {
+                    logLengthDouble = Double.parseDouble(logLengthString);
 
-            // If else statement for invalid log length
-            if (logLengthDouble != 0.25 || logLengthDouble != 0.5 || logLengthDouble || 1) {
-                System.out.println("Invalid input, "+ logLengthString + " log length must be 0.25, 0.5, or 1.");
-            } else {
-                // Calculate the weight per log
-                final double weightPerLog = 20 * logLengthDouble;
+                    // Validate input
+                    if (logLengthDouble == 0.25
+                    || logLengthDouble == 0.5 || logLengthDouble == 1) {
+                        // Break the loop if the input is valid
+                        break;
+                    } else {
+                        // Error message for invalid input
+                        System.out.println("Invalid input: " + logLengthString
+                        + ". Log length must be 0.25, 0.5, or 1.");
+                    }
+                } catch (NumberFormatException error) {
+                    // Error message for invalid input
+                    System.out.println("Invalid input: " + logLengthString
+                    + ". Log length must be 0.25, 0.5, or 1.");
+                }
+            // Infinite loop that only breaks when a valid input is given
+            } while (true);
 
-                // Calculate the number of logs
-                final double numLogsDouble = MAX_WEIGHT / weightPerLog;
+            // Calculate the weight per log
+            final double weightPerLog = 20 * logLengthDouble;
 
-                // Convert the number of logs to an integer
-                final int numLogsInt = (int) numLogsDouble;
+            // Calculate the number of logs
+            final int numLogsInt = (int) (MAX_WEIGHT / weightPerLog);
 
-                // Display the max number of logs
-                System.out.print("The truck will be able to take ");
-                System.out.print(numLogsInt + " logs that are ");
-                System.out.format(logLengthDouble + "m(s) in length.");
-            }
-        } catch (NumberFormatException error) {
-            // error message
-            System.out.println("Invalid input. Please enter a valid log length."
-                    + error.getMessage());
+            // Display the max number of logs
+            System.out.println("The truck will be able to take " + numLogsInt
+            + " logs that are " + logLengthDouble + "m(s) in length.");
         }
-
-        // Close the scanner
-        scanner.close();
     }
 }
